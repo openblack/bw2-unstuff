cb2e4e3f95adb9c00bd440d6021036e08ce6008f
Matt Stevens <matt@handsomematt.co.uk>
2017-01-20 11:59:59 +0000

    .NET Framework Console App project.

---
+++ BW2Unstuff/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BW2Unstuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
